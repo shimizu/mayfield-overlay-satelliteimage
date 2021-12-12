029630c5bc71de582f815a8edb877c698c5262a4
shimizu <xxshimizuxx@gmail.com>
2021-12-12 10:06:53 +0900

    画像圧縮

--- src/Components/Map/RenderLayers.js
+++ src/Components/Map/RenderLayers.js
@@ -7,7 +7,7 @@ export function renderLayers({value}) {
   const layer = new BitmapLayer({
     id: 'bitmap-layer',
     bounds: [-88.64538, 36.73265, -88.62293, 36.75064],
-    image: 'img/mayfield-2021-12-11.jpg',
+    image: 'img/mayfield-2021-12-11-min.jpg',
     opacity: opacity
   });
 
